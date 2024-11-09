b2b3e86d647237f6ae49661c13bcc44e02e48a59
viraj patel <141030887+Viraj1635@users.noreply.github.com>
2024-11-09 17:57:56 +0530

    landing pages and message pages

---
+++ lib/Components/messageReqDiscItem.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class MessageReqDiscItem extends StatelessWidget {
+  final String text;
+  final String messageText;
+  final IconData icon;
+
+  const MessageReqDiscItem({
+    required this.text,
+    required this.messageText,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: screenWidth * 0.2,
+            height: screenWidth * 0.2,
+            decoration: BoxDecoration(
+              color: Colors.blue.shade100,
+              shape: BoxShape.circle,
+            ),
+            child: Center(
+              child: Icon(
+                icon,
+                color: Colors.blue,
+                size: screenWidth * 0.1,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 12,
+        ),
+        Text(
+          text,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
+            fontSize: screenWidth * 0.04,
+          ),
+        ),
+        SizedBox(
+          width: 12,
+          height: screenHeight * 0.05,
+        ),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            padding: EdgeInsets.all(screenWidth * 0.03),
+          ),
+          onPressed: () {},
+          child: Center(
+            child: Text(
+              messageText,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: screenWidth * 0.04,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/messageReqDisc.dart
@@ -0,0 +1,60 @@
+import 'package:link_up/Components/messageReqDiscItem.dart';
+import 'package:flutter/material.dart';
+
+class MessageReqDiscScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Color(0xFF0F1828),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.only(top: 40, bottom: 10, left: 16, right: 16),
+              padding: EdgeInsets.all(10),
+              width: screenWidth * 0.9,
+              height: screenHeight * 0.4,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: const Color(0xFF375FFF),
+                  width: 1,
+                ),
+                color: Color(0xFF152033),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: MessageReqDiscItem(
+                icon: Icons.chat_bubble,
+                text: 'Connect with your friends\n over local areas',
+                messageText: 'Message Request',
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.only(top: 10, bottom: 40, left: 16, right: 16),
+              padding: EdgeInsets.all(10),
+              width: screenWidth * 0.9,
+              height: screenHeight * 0.4,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: const Color(0xFF375FFF),
+                  width: 1,
+                ),
+                color: Color(0xFF152033),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: MessageReqDiscItem(
+                icon: Icons.person_add,
+                text: 'Discover your new friends\n over local areas',
+                messageText: 'Message Request',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
